dbc3a9c205ea659292f5efce4a21fb5c9034260d
zafird <zafird95@gmail.com>
2019-10-10 11:00:43 -0700

    Initial commit

---
+++ app/src/main/java/ca/bcit/comp3717project/FullView.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_view);
+    }
+}
---
+++ app/src/main/java/ca/bcit/comp3717project/Search.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
