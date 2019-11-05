9ad66e7e856df52c6b947af068b74e50c1885134
Jameson Cheong <jctheathlete@gmail.com>
2019-11-04 21:31:17 -0800

    commit now

--- app/src/main/java/ca/bcit/comp3717project/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717project/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bottom_navigation, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.iHome:
+                // User chose the "Settings" item, show the app settings UI...
+                System.out.println("Menu Item Home");
+                return true;
+            case R.id.iMap:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                System.out.println("Menu Item Map");
+                goToMapActivity();
+                return true;
+            case R.id.iBook:
+                System.out.println("Menu Item Book");
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
 
-    public void maps(View view){
+        }
+    }
+
+    public void goToMapActivity(){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
